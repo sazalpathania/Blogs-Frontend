e8e77693d195334b2c834761f60892dcc857c2e5
Sazal <sazalpathania.777@gmail.com>
2025-12-24 14:01:54 +0530

    next commit

--- src/components/blogCard/blog.card.component.jsx
+++ src/components/blogCard/blog.card.component.jsx
@@ -1,25 +1,35 @@
 import "./blog.style.css";
 
-const BlogCard = ({ blog, handleDelete, showDelete = false }) => {
+const BlogCard = ({
+  blog,
+  handleDelete,
+  showDelete = false,
+  showEdit,
+  onEdit,
+}) => {
   return (
-    <div className="blog-card">
-      <h4>{blog.title}</h4>
+    <>
+      <div className="blog-card">
+        <h4>{blog.title}</h4>
 
-      <img src={`http://localhost:4000${blog.image}`} alt={blog.image} />
+        <img src={`http://localhost:4000${blog.image}`} alt={blog.image} />
 
-      <p>{blog.content.slice(0, 100)}...</p>
+        <p>{blog.content.slice(0, 100)}...</p>
 
-      <div className="author">
-        <span>{blog.author?.username}</span>
-        <small>{new Date(blog.createdAt).toDateString()}</small>
+        <div className="author">
+          <span>{blog.author?.username}</span>
+          <small>{new Date(blog.createdAt).toDateString()}</small>
+        </div>
+
+        {showEdit && <button onClick={() => onEdit(blog)}>Edit</button>}
+
+        {showDelete && (
+          <button className="delete-btn" onClick={() => handleDelete(blog._id)}>
+            Delete
+          </button>
+        )}
       </div>
-
-      {showDelete && (
-        <button className="delete-btn" onClick={() => handleDelete(blog._id)}>
-          Delete
-        </button>
-      )}
-    </div>
+    </>
   );
 };
 
--- src/pages/myBlog/myBlog.jsx
+++ src/pages/myBlog/myBlog.jsx
@@ -7,6 +7,16 @@ import "./myBlog.style.css";
 const MyBlogs = () => {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [editing, setEditing] = useState(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleEditClick = (blog) => {
+    setEditing(blog);
+    setTitle(blog.title);
+    setDescription(blog.content);
+  };
 
   //Fetching Blogs//
   const fetchBlogs = async () => {
@@ -36,6 +46,35 @@ const MyBlogs = () => {
     fetchBlogs();
   }, []);
 
+  //Edit handler//
+
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+      const formData = new FormData();
+
+      formData.append("title", title);
+      formData.append("description", description);
+      if (image) formData.append("file", image);
+
+      const res = await axios.put(`${API_URL2}/edit/${editing._id}`, formData, {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      setBlogs(blogs.map((n) => (n._id === editing._id ? res.data.blog : n)));
+
+      setEditing(null);
+
+      alert("Blog updated successfully");
+    } catch (error) {
+      console.log(error);
+      alert("You are not allowed to edit this blog");
+    }
+  };
+
   //Delete handler//
   const handleDelete = async (blogID) => {
     try {
@@ -57,20 +96,67 @@ const MyBlogs = () => {
   if (!blogs.length) return <p>You have not created any blogs yet</p>;
 
   return (
-    <div className="my-blogs-container">
-      <h2>My Blogs</h2>
+    <>
+      <div className="my-blogs-container">
+        <h2>My Blogs</h2>
 
-      <div className="blog-grid">
-        {blogs.map((blog) => (
-          <BlogCard
-            key={blog._id}
-            blog={blog}
-            handleDelete={handleDelete}
-            showDelete={true}
-          />
-        ))}
+        <div className="blog-grid">
+          {blogs.map((blog) => (
+            <BlogCard
+              key={blog._id}
+              blog={blog}
+              handleDelete={handleDelete}
+              showDelete={true}
+              showEdit={true}
+              onEdit={handleEditClick}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+
+      {editing && (
+        <div className="modal">
+          <form onSubmit={handleEditSubmit} className="add-blog-form">
+            <h3>Edit Blog</h3>
+
+            <input
+              className="input-field"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              required
+            />
+
+            <textarea
+              className="textarea-field"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+              required
+            />
+
+            <label>Upload Image</label>
+            <div className="upload-box">
+              <input
+                className="file-input"
+                type="file"
+                accept="image/*"
+                onChange={(e) => setImage(e.target.files[0])}
+              />
+              <span>Upload here</span>
+            </div>
+
+            <div className="modal-actions">
+              <button type="submit">Update</button>
+              <button type="button" onClick={() => setEditing(null)}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
