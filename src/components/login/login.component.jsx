a5ef1296b8a344dea6c78ecfa4b39998b60ed5e8
Sazal <sazalpathania.777@gmail.com>
2026-01-11 13:15:16 +0530

    next commit

--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -19,7 +19,7 @@ export const Login = () => {
     setError("");
     setIsLoading(true);
     try {
-      const res = await axios.post(`${API_URL}/login`, {
+      const res = await axios.post(`auth/login`, {
         email,
         password,
       });
